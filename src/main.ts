36417d7d40829cc6cf4091d952f792604e8d90ba
AgathePons <ponsagathe37@gmail.com>
2022-10-28 10:35:11 +0200

    ADD: start entry in npm scripts

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+/**
+ * main.ts : Entry point in oop-ts application
+ * @author AgathePons 2022-10
+ * @version 1.0.0
+ */
+
+export class Main {
+    public constructor() {
+        console.log('Hello TS');
+    }
+}
+
+new Main();
